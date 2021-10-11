47fb5f03a16717a98660dd381c4d4edf38474934
Apesosmarc <73441355+Apesosmarc@users.noreply.github.com>
2021-10-11 13:43:53 -0400

    Responsive header, mobile contact form

---
+++ splash.js
@@ -0,0 +1,9 @@
+const splash = document.querySelector(".splash");
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
+    splash.classList.add(".display-none");
+  }, 1000);
+});
+
+// https://webdesign.tutsplus.com/tutorials/quick-tip-how-to-enhance-a-page-with-css-animations--cms-32100
